1c8657232756d4f849290ba263a23512f3c15409
Ajay-Bankar <bankarajay116@gmail.com>
2025-01-28 14:05:57 +0530

    header update with logo ang bg color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,21 +32,21 @@ const Header = () => {
     <>
       {isSmallScreen ? (
         // Mobile Header
-        <div className='flex justify-between items-center px-4 fixed top-0 w-full h-[60px] bg-gray-300 z-50 shadow-md'>
+        <div className='flex justify-between items-center px-4 fixed top-0 w-full h-[60px] bg-green-900 z-50 shadow-md'>
           <button onClick={() => setIsSidebarOpen(true)} className='absolute left-4'>
-            <IoIosMenu size={30} />
+            <IoIosMenu size={30} color='#e9ecef' />
           </button>
           
           <div className='w-full flex justify-center'>
             <img 
-              className='w-[150px] h-[20px] object-cover' 
-              src="https://images.ctfassets.net/afluk6wxa60f/7HhDnKTmFakdqUVoQdfnIE/3c9c7e2eda4fce39459b93e06c2f4a5d/Tyroola_logo_185x185.png" 
-              alt="Tyroola Logo" 
+              className='w-[150px]  object-cover' 
+              src="../images/nova-logo.png" 
+              alt="Nova Logo" 
             />
           </div>
           
           <div className='absolute right-4 '>
-            <IoCartOutline size={30} />
+            <IoCartOutline size={30} color='#e9ecef' />
             <div className='absolute -top-2 -right-2 flex justify-center items-center bg-yellow-500 text-white rounded-full w-5 h-5 border border-black text-xs'>
               {count}
             </div>
@@ -62,14 +62,14 @@ const Header = () => {
                 className='fixed top-0 left-0 w-[80%] h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out'
                 onClick={(e) => e.stopPropagation()}
               >
-                <div className='flex justify-between items-center p-4 border-b'>
+                <div className='flex justify-between bg-green-900 items-center p-4 border-b'>
                   <img 
-                    className='w-[120px] h-[16px] object-cover' 
-                    src="https://images.ctfassets.net/afluk6wxa60f/7HhDnKTmFakdqUVoQdfnIE/3c9c7e2eda4fce39459b93e06c2f4a5d/Tyroola_logo_185x185.png" 
-                    alt="Tyroola Logo" 
+                    className='w-[120px] h-[32px] object-cover' 
+                    src="../images/nova-logo.png" 
+                    alt="Nova Logo" 
                   />
                   <button onClick={() => setIsSidebarOpen(false)}>
-                    <IoIosClose size={30} />
+                    <IoIosClose size={30} color='#e9ecef'  />
                   </button>
                 </div>
                 
@@ -91,21 +91,21 @@ const Header = () => {
         </div>
       ) : (
         // Desktop Header
-        <div className='flex justify-between items-center px-8 fixed top-0 w-full h-[60px] bg-gray-400 z-50 shadow-md'>
+        <div className='flex justify-between items-center px-8 fixed top-0 w-full h-[60px] bg-green-900 z-50 shadow-md'>
           <div className='w-full flex justify-center gap-8 items-center'>
             <div>
               <Link to='/'>
                 <img 
-                  className='w-[150px] h-[20px] object-cover' 
-                  src="https://images.ctfassets.net/afluk6wxa60f/7HhDnKTmFakdqUVoQdfnIE/3c9c7e2eda4fce39459b93e06c2f4a5d/Tyroola_logo_185x185.png" 
-                  alt="Tyroola Logo" 
+                  className=' object-cover' 
+                  src="../images/nova-logo.png" 
+                  alt="Nova Logo" 
                 />
               </Link>
             </div>
             {menuItems.slice(0, 4).map((item, index) => (
               <div key={index} className='flex justify-center items-center gap-2'>
-                <Link to={item.path}>{item.label}</Link>
-                <p><IoIosArrowDown color='#000000' /></p>
+                <Link className=' text-gray-200 font-semibold text-[1rem]' to={item.path}>{item.label}</Link>
+                <p><IoIosArrowDown color='#e9ecef' /></p>
               </div>
             ))}
           </div>
@@ -113,12 +113,12 @@ const Header = () => {
           <div className='w-full flex justify-end gap-8 items-center'>
             {menuItems.slice(4).map((item, index) => (
               <div key={index} className='flex justify-center items-center gap-2'>
-                <p>{item.label}</p>
-                <p><IoIosArrowDown color='#000000' /></p>
+                <p className='text-gray-200 font-semibold text-[1rem]'>{item.label}</p>
+                <p><IoIosArrowDown color='#e9ecef' /></p>
               </div>
             ))}
             <div className='relative flex justify-center items-center gap-2'>
-              <p><IoCartOutline size={30} /></p>
+              <p><IoCartOutline size={30} color='#e9ecef' /></p>
               <div className='absolute -top-2 -right-2 flex justify-center items-center bg-yellow-500 text-white rounded-full w-6 h-6 border border-black'>
                 {count}
               </div>
