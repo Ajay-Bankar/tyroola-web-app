6c535bbd1533af0982b4b97c1c901d464d82de92
Ajay-Bankar <bankarajay116@gmail.com>
2025-01-28 13:03:35 +0530

    header push with bg color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
     <>
       {isSmallScreen ? (
         // Mobile Header
-        <div className='flex justify-between items-center px-4 fixed top-0 w-full h-[60px] bg-white z-50 shadow-md'>
+        <div className='flex justify-between items-center px-4 fixed top-0 w-full h-[60px] bg-gray-300 z-50 shadow-md'>
           <button onClick={() => setIsSidebarOpen(true)} className='absolute left-4'>
             <IoIosMenu size={30} />
           </button>
